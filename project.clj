900e2a1a5605029454ec78ee16400494061abb50
Peter Denno <podenno@gmail.com>
2017-10-25 15:28:00 -0400

    New project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject pdenno/nn "0.1.0"
+  :description "Simple feed-forward neural net example"
+  :url "https://github.com/pdenno"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]]
+  :main pdenno.nn)
